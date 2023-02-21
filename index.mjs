e19b86108985f323976ecde793cb6721818daefe
Lily Halladay <lilyhalladay14@gmail.com>
2023-02-21 10:26:30 +0000

    added prompt questions for user

--- index.mjs
+++ index.mjs
@@ -2,5 +2,44 @@ import inquirer from 'inquirer';
 
 inquirer 
 .prompt([
-       
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Please provide a description of your project:',
+      },   
+      {
+        type: 'input',
+        name: 'installationInstructions',
+        message: 'Please provide installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage instructions:',
+      },
+      {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please list any contributors to the project:',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'What license will you use for your project?',
+      },
+      {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
 ])
